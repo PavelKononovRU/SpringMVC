ac454277fb88fa018620891988e144dfd654e1db
Pavel <lovecars527@gmail.com>
2022-11-05 21:48:46 +0300

    WOrk!!

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,7 +3,9 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +13,7 @@ import java.util.List;
 public class CarController {
 
     @GetMapping(value = "/cars")
-    public String printCars(ModelMap model) {
+    public String printCars(@RequestParam(value = "count", required = false) int count, ModelMap model) {
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("BMW", "3er", 192));
         cars.add(new Car("Toyota", "Camry", 275));
@@ -19,7 +21,7 @@ public class CarController {
         cars.add(new Car("Renault", "Megane", 113));
         cars.add(new Car("Chevrolet", "Captiva", 182));
 
-        model.addAttribute("car", cars);
+        model.addAttribute("car", CarService.getCarsList(count, cars));
         return "car";
     }
 }
